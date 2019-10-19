d71bd1abfcbde6312c026dcf0f4ff9658a7be7c5
CaoGuangQing <1156888043@qq.com>
2019-10-19 12:11:40 +0800

    Initial commit

---
+++ Module01/src/cn/edu/nyist/test/First.java
@@ -0,0 +1,7 @@
+package cn.edu.nyist.test;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("ttt------------");
+    }
+}
---
+++ src/test/Hello.java
@@ -0,0 +1,10 @@
+package test;
+
+import cn.edu.nyist.test.Goods;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        new Goods();
+    }
+}
